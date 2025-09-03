dcce8b1554faad570f5a74c20f03a013bc39c997
Ethan Posner <Enprogames@users.noreply.github.com>
2025-09-03 07:35:58 -0700

    refactor: remove hobbies page and revamp about layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Container, Card, CardContent, Typography } from '@mui/material';
+import { aboutContent } from '../Assets/about';
 
 const About: React.FC = () => {
-  return <div>This is the About page</div>;
+  return (
+    <Container maxWidth="md" className="py-6">
+      <Card className="shadow">
+        <CardContent>
+          <Typography variant="h4" gutterBottom>
+            {aboutContent.title}
+          </Typography>
+          {aboutContent.paragraphs.map((text, idx) => (
+            <Typography key={idx} paragraph>
+              {text}
+            </Typography>
+          ))}
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default About;
--- src/pages/Hobbies.tsx
+++
@@ -1,8 +0,0 @@
-// pages/Hobbies.tsx
-import React from 'react';
-
-const Hobbies: React.FC = () => {
-  return <div>This is the Hobbies page</div>;
-};
-
-export default Hobbies;
