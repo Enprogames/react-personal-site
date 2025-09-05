257680d88abde3cc87162c71aa9b0b6fa6b2e256
Ethan Posner <e_posner@outlook.com>
2025-09-04 19:35:13 -0700

    Fixed failing tests in pipeline

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,3 +4,12 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+// Polyfill requestAnimationFrame for libraries that expect it in JSDOM
+if (!(globalThis as any).requestAnimationFrame) {
+  (globalThis as any).requestAnimationFrame = (cb: FrameRequestCallback) =>
+    setTimeout(() => cb(performance.now()), 16) as unknown as number;
+}
+
+if (!(globalThis as any).cancelAnimationFrame) {
+  (globalThis as any).cancelAnimationFrame = (id: number) => clearTimeout(id);
+}
